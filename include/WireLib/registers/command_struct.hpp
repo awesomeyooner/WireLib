cf93352ae11c6fa72d678aa8087e12f9eaad7e3b
Susan Yoon <awesomeyooner@gmail.com>
2026-01-14 18:18:17 -0800

    Finished rewriting the Command Struct

--- include/WireLib/registers/command_struct.hpp
+++ include/WireLib/registers/command_struct.hpp
@@ -4,12 +4,47 @@
 #include <cstdint>
 #include <functional>
 
+#include "EmbeddedLib/util/status.hpp"
+
+#include "WireLib/util/byte_converter.hpp"
+
+/**
+ * @brief Commands are sent TO the microcontroller (what you're writing the firmware on).
+ * For example, a command can be a host can send a command to the microcontroller to
+ * run the motor at the desired velocity, send via a data packet
+ * 
+ */
 struct Command
 {
-    uint8_t reg;
-    int length;
-    std::function<StatusCode(std::vector<uint8_t>*)> runnable;
+    // The register byte
+    uint8_t m_reg;
 
-}
+    // The length of the data packet to recieve
+    int m_length;
+
+    // What runs after the register is called
+    std::function<status_utils::StatusCode(std::vector<uint8_t>*)> m_runnable;
+
+    /**
+     * @brief Makes a command that takes in incoming bytes as a `float` and sets it to `value`.
+     * Also runs an optional runnable once the register is called
+     * 
+     * @param reg `uint8_t` The register this is associated with
+     * @param value `float&` The reference to the value to set
+     * @param runnable `std::function<void()>` Runnable to run when the command is recieved
+     * @return `Command` The associated command struct 
+     */
+    static Command make_float(uint8_t reg, float* value, std::function<void()> runnable = NULL);
+
+    /**
+     * @brief Makes a command that runs the runnable when the register is called
+     * 
+     * @param reg `uint8_t` The register this is associated with
+     * @param runnable `std::function<void()>` Runnable to run whent he command is recieved
+     * @return `Command` The associated command struct 
+     */
+    static Command make_runnable(uint8_t reg, std::function<void()> runnable);
+
+}; // struct Command
 
 #endif // COMMAND_STRUCT_HPP
---
+++ src/WireLib/registers/command_struct.cpp
@@ -0,0 +1,50 @@
+#include "WireLib/registers/command_struct.hpp"
+
+Command Command::make_float(uint8_t reg, float* value, std::function<void()> runnable)
+{
+    return Command{
+        // Set the register equal to the register
+        .m_reg = reg,
+
+        // The length of the data packet to recieve is the size of a float (4 bytes)
+        .m_length = sizeof(float),
+
+        // The runnable will take in the incoming data, convert it to a float,
+        // set the pointer's float value equal to that value, and run the runnable
+        .m_runnable = [value, runnable](std::vector<uint8_t>* data) -> status_utils::StatusCode
+        {
+            // If the runnable isn't NULL, then run it
+            if(runnable)
+                runnable();
+
+            // Convert the incoming bytes into a float
+            float f_data = ByteConverter::bytes_to_float(*data);
+
+            // Set the pointer's value equal to the incoming float
+            *value = f_data;
+
+            // Return OK
+            return status_utils::StatusCode::OK;
+        }
+    };
+
+} // end of "make_float"
+
+
+Command Command::make_runnable(uint8_t reg, std::function<void()> runnable)
+{
+    return Command{
+        // Set the register equal to the register
+        .m_reg = reg,
+
+        // 0 since we aren't recieving any data
+        .m_length = 0,
+        
+        // Run the runnable and return OK
+        .m_runnable = [runnable](std::vector<uint8_t>* data) -> status_utils::StatusCode{
+            runnable();
+            return status_utils::StatusCode::OK;
+        }
+    };
+    
+} // end of "make_runnable"
