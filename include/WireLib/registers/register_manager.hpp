de909f27da610570d97eb0f62bfd8e088ed3073d
Susan Yoon <awesomeyooner@gmail.com>
2026-01-14 19:45:51 -0800

    Finished writing register manager

---
+++ include/WireLib/registers/register_manager.hpp
@@ -0,0 +1,92 @@
+#ifndef REGISTER_MANAGER_HPP
+#define REGISTER_MANAGER_HPP
+
+#include <map>
+#include <unordered_map>
+
+#include "WireLib/registers/command_struct.hpp"
+#include "WireLib/registers/request_struct.hpp"
+
+#include "WireLib/util/byte_converter.hpp"
+
+#include "EmbeddedLib/util/status.hpp"
+
+
+class RegisterManager
+{
+
+    public:
+
+        // Buffer that contains all the bytes read from the host
+        static std::vector<uint8_t> m_read_buffer;
+
+        // Buffer that contains all the bytes to write to the host
+        static std::vector<uint8_t> m_write_buffer;
+
+        /**
+         * @brief Runs the register associated with `reg` if it exists. Returns FAILED if it doesn't.
+         * 
+         * @param reg `uint8_t` The register to call
+         * @param incoming_data `std::vector<uint8_t>*` The incoming data,
+         * MUST be populated ONLY with the data to parse, it should not include the register byte
+         * @return `status_utils::StatusCode` FAILED if neither maps contain the associated register or
+         * if the runnable inside the register fails. OK otherwise 
+         */
+        static status_utils::StatusCode update(uint8_t reg, std::vector<uint8_t>* incoming_data);
+
+        /**
+         * @brief Gets the pointer to the read buffer
+         * 
+         * @return `std::vector<uint8_t>*` The pointer to the read buffer 
+         */
+        static std::vector<uint8_t>* get_read_buffer();
+
+        /**
+         * @brief Gets the pointer to the write buffer
+         * 
+         * @return `std::vector<uint8_t>*` The pointer to the write buffer 
+         */
+        static std::vector<uint8_t>* get_write_buffer();
+
+        /**
+         * @brief Add a request to the request map
+         * 
+         * @param request `Request` The request to add
+         */
+        static void add_request(Request request);
+
+        /**
+         * @brief Add a request to the request map
+         * 
+         * @param reg `uint8_t` The register
+         * @param length `int` The length of the data to send in bytes
+         * @param runnable `std::function<status_utils::StatusCode()>` The runnable that will send the data
+         */
+        static void add_request(uint8_t reg, int length, std::function<status_utils::StatusCode()> runnable);
+
+        /**
+         * @brief Add a command to the command map
+         * 
+         * @param command `Command` The command to add
+         */
+        static void add_command(Command command);
+
+        /**
+         * @brief Add a command to the command map
+         * 
+         * @param reg `uint8_t` The register
+         * @param length `int` The length of the data to recieve in bytes
+         * @param runnable `std::function<status_utils::StatusCode(std::vector<uint8_t>*)>` The runnable that will use 
+         * the recieved bytes
+         */
+        static void add_command(uint8_t reg, int length, std::function<status_utils::StatusCode(std::vector<uint8_t>*)> runnable);
+
+    private:
+
+        // Maps to get the request / command associated with a register (uint8_t)
+        static std::unordered_map<uint8_t, Request> m_request_map;
+        static std::unordered_map<uint8_t, Command> m_command_map;
+
+}; // class RegisterManager
+
+#endif // REGISTER_MANAGER_HPP
---
+++ src/WireLib/registers/register_manager.cpp
@@ -0,0 +1,90 @@
+#include "WireLib/registers/register_manager.hpp"
+
+
+// Initialize the static members
+std::vector<uint8_t> RegisterManager::m_read_buffer;
+std::vector<uint8_t> RegisterManager::m_write_buffer;
+
+std::unordered_map<uint8_t, Request> RegisterManager::m_request_map;
+std::unordered_map<uint8_t, Command> RegisterManager::m_command_map;
+
+
+status_utils::StatusCode RegisterManager::update(uint8_t reg, std::vector<uint8_t>* incoming_data)
+{
+    // If both the maps are empty, return FAILED
+    if(m_request_map.empty() && m_command_map.empty())
+        return status_utils::StatusCode::FAILED;
+
+    // If the register exists, call its runnable
+    if(m_request_map.find(reg) != m_request_map.end()){
+        // Serial.println("Request Map Called");
+        return m_request_map.at(reg).m_runnable();
+    }
+    
+    // If the register exists, call its runnable
+    if(m_command_map.find(reg) != m_command_map.end()){
+        // Serial.println("Command Map Called");
+        return m_command_map.at(reg).m_runnable(incoming_data);
+    }
+
+    return status_utils::StatusCode::FAILED;
+
+} // end of "update"
+
+
+std::vector<uint8_t>* RegisterManager::get_read_buffer()
+{
+    return &m_read_buffer;
+
+} // end of "get_read_buffer"
+
+
+std::vector<uint8_t>* RegisterManager::get_write_buffer()
+{
+    return &m_write_buffer;
+
+} // end of "get_write_buffer"
+
+
+void RegisterManager::add_request(Request request)
+{
+    // register, request
+    m_request_map.insert({request.m_reg, request});
+
+} // end of "add_request"
+
+
+void RegisterManager::add_request(uint8_t reg, int length, std::function<status_utils::StatusCode()> runnable)
+{
+    Request request = 
+    {
+        .m_reg = reg,
+        .m_length = length,
+        .m_runnable = runnable
+    };
+
+    add_request(request);
+
+} // end of "add_request"
+
+
+void RegisterManager::add_command(Command command)
+{
+    // register, command
+    m_command_map.insert({command.m_reg, command});
+
+} // end of "add_command"
+
+
+void RegisterManager::add_command(uint8_t reg, int length, std::function<status_utils::StatusCode(std::vector<uint8_t>*)> runnable)
+{
+    Command command = 
+    {
+        .m_reg = reg,
+        .m_length = length,
+        .m_runnable = runnable
+    };
+
+    add_command(command);
+
+} // end of "add_command"
