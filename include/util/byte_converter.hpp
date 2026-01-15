ba3be051a26502579dc3acacf4bdd7c2841b5e7f
Susan Yoon <awesomeyooner@gmail.com>
2026-01-14 17:05:41 -0800

    Documented the byte_converter methods

--- include/util/byte_converter.hpp
+++ include/util/byte_converter.hpp
@@ -18,8 +18,8 @@ union DoublesBytesConverter{
 };
 
 // You must declare unions like this and define them in .cpp
-extern FloatsBytesConverter f_to_b;
-extern DoublesBytesConverter d_to_b;
+extern FloatsBytesConverter f_to_b; // float to bytes
+extern DoublesBytesConverter d_to_b; // double to bytes
 
 
 class ByteConverter
@@ -27,10 +27,38 @@ class ByteConverter
 
     public:
 
+        /**
+         * @brief Converts a vector of 4 bytes into a float 
+         * 
+         * @param v_bytes `std::vector<uint8_t>` The vector of 4 bytes 
+         * @return `float` The converted float value 
+         */
         static float bytes_to_float(std::vector<uint8_t> v_bytes);
+
+        /**
+         * @brief Converts a float into a vector of 4 bytes
+         * 
+         * @param value `float` The float value to convert
+         * @return `std::vector<uint8_t>` A vector of 4 bytes
+         * representing the float 
+         */
         static std::vector<uint8_t> float_to_bytes(float value);
 
+        /**
+         * @brief Converts a vector of 8 bytes into a double
+         * 
+         * @param v_bytes `std::vector<uint8_t>` The vector of 8 bytes 
+         * @return `double` The converted double value 
+         */
         static double bytes_to_double(std::vector<uint8_t> v_bytes);
+
+        /**
+         * @brief Converts a double into a vector of 8 bytes
+         * 
+         * @param value `double` The double value to convert
+         * @return `std::vector<uint8_t>` A vector fo 8 bytes
+         * representing the double 
+         */
         static std::vector<uint8_t> double_to_bytes(double value);
 
 }; // class ByteConverter
