ff7db1fe732ca8f43d6c234a797da1c2a054b8ad
Susan Yoon <awesomeyooner@gmail.com>
2026-01-15 21:45:14 -0800

    Updated documentation for wire_manager

--- include/WireLib/communication/wire_manager.hpp
+++ include/WireLib/communication/wire_manager.hpp
@@ -10,10 +10,26 @@ class WireManager
 
     public:
 
+        /**
+         * @brief Initializes the I2C bus and links the 
+         * on_recieve and on_request functions to the callbacks
+         * 
+         * @param address `int` The address this device is associated with
+         */
         static void initialize(int address);
 
+        /**
+         * @brief Callback function for what to do when the host sends data.
+         * Internally, it stores the data in the read buffer and updates the registers
+         * @param num_bytes `int` The number of bytes to read
+         */
         static void on_recieve(int num_bytes);
 
+        /**
+         * @brief Callback function for what to do when the host requests data.
+         * Internally, it writes whatevers in the write buffer
+         * 
+         */
         static void on_request();
 
     private:
