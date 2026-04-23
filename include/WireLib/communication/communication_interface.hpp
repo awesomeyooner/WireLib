378f66fda0e9475e48ceba8df8d1bc545f523916
Aaron Yoon <awesomeyooner@gmail.com>
2026-04-23 09:44:09 -0700

    Added WireState to describe the current processing state of the read buffer

--- include/WireLib/communication/communication_interface.hpp
+++ include/WireLib/communication/communication_interface.hpp
@@ -7,6 +7,22 @@
 #include <vector>
 #include <functional>
 
+#include "EmbeddedLib/util/status.hpp"
+
+using namespace status_utils;
+
+
+/**
+ * @brief Describes the state of the communication (wire) interface
+ * 
+ */
+enum class WireState
+{
+    READY,  // The stack is ready to recieve a new packet 
+    ONGOING,  // The stack is currently recieving packets and is waiting for the rest of the packets to arrive  
+    FINISHED  // The stack has recieved all of the needed packets
+};
+
 
 class CommunicationInterface
 {
@@ -15,13 +31,13 @@ class CommunicationInterface
 
         void configure_on_receive(std::function<bool(const std::vector<uint8_t>&)> callback);
 
-        virtual bool on_receive(uint8_t* buffer, uint32_t length);
+        virtual StatusCode on_receive(uint8_t* buffer, uint32_t length);
 
-        virtual bool transmit_bytes(const std::vector<uint8_t>& bytes);
+        virtual StatusCode transmit_bytes(const std::vector<uint8_t>& bytes);
 
     private:
 
-        std::function<bool(const std::vector<uint8_t>&)> m_receive_callback;
+        std::function<StatusCode(const std::vector<uint8_t>&)> m_receive_callback;
 
         std::vector<uint8_t> m_read_buffer;
 
