b3aa84f3665384c61635696a41c3d52782b7dd92
Aaron Yoon <awesomeyooner@gmail.com>
2026-09-14 21:07:20 -0700

    Started working on I2CInterface

---
+++ include/WireLib/communication/protocols/i2c/i2c_interface.hpp
@@ -0,0 +1,18 @@
+#ifndef I2C_INTERFACE_HPP
+#define I2C_INTERFACE_HPP
+
+
+#include "i2c.h"
+
+#include "EmbeddedLib/system.hpp"
+#include "EmbeddedLib/util/string_formatter.hpp"
+
+#include "WireLib/communication/communication_interface.hpp"
+
+#include <string>
+#include <vector>
+#include <functional>
+
+
+
+#endif // I2C_INTERFACE_HPP
